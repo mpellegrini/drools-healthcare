7d003b188715b8a49e33ec381d980dd669943b8b
Michael Pellegrini <mpellegrini@kayasoftware.com>
2014-04-22 08:32:50 -0400

    Change rounding code to use BigDecimal with setScale

--- src/main/java/com/michaelpellegrini/drools/healthcare/DroolsHelper.java
+++ src/main/java/com/michaelpellegrini/drools/healthcare/DroolsHelper.java
@@ -4,23 +4,28 @@ import javax.measure.Measurable;
 import javax.measure.quantity.Length;
 import javax.measure.quantity.Mass;
 import javax.measure.unit.SI;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class DroolsHelper {
-	
-	public static double calculateBMI(Measurable<Length> height, Measurable<Mass> weight) {
-		/**
-		** English BMI Formula
-		** BMI = ( Weight in Pounds / ( Height in inches ) x ( Height in inches ) ) x 703
-		** Metric BMI Formula
-		** BMI = ( Weight in Kilograms / ( Height in Meters ) x ( Height in Meters ) )
-		*/
-		double bmi = weight.doubleValue(SI.KILOGRAM) / (height.doubleValue(SI.METRE) * height.doubleValue(SI.METRE));
 
-		// Rounding double 
-		float p = (float) Math.pow(10, 1);
-		bmi = bmi * p;
-		double tmp = Math.round(bmi);
-		
-		return tmp/p;
+    /**
+     * English BMI Formula
+     * BMI = ( Weight in Pounds / ( Height in inches ) x ( Height in inches ) ) x 703
+     *
+     * Metric BMI Formula
+     * BMI = ( Weight in Kilograms / ( Height in Meters ) x ( Height in Meters ) )
+     *
+     * @param height
+     * @param weight
+     * @return
+     */
+    public static double calculateBMI(final Measurable<Length> height, final Measurable<Mass> weight) {
+        double bmi = weight.doubleValue(SI.KILOGRAM) / (height.doubleValue(SI.METRE) * height.doubleValue(SI.METRE));
+
+        // Round double to one decimal place
+        BigDecimal bigDecimal = new BigDecimal(bmi).setScale(1, RoundingMode.HALF_UP);
+
+        return bigDecimal.doubleValue();
 	}
 }
